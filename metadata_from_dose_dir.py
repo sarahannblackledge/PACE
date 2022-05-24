a0f8d1f7e8db04353a31f8419e25ae96051b5995
Sarah Blackledge <sblackledge@25283radlt.icr.ac.uk>
2022-05-24 14:03:54 +0100

    separated PACE and PRISM plans so output is now two dictionaries

--- metadata_from_dose_dir.py
+++ metadata_from_dose_dir.py
@@ -2,16 +2,18 @@ import numpy as np
 import os
 import SimpleITK as sitk
 
-#Create dictionary indicating image id and plan corresponding to each filename for all mha files in specified directory
+#Create separate dictionaries for PACE and PRISM plans within user-specified directory with filepath and relevant metadata as key-value pairs
 #inputs:
     #1. dose_dir: string - full file path of directory where mha files are stored
 
 #Output:
-    #1. python dictionary: keys = filepath of mha files, values = [image id number, plan]
+    #1. PACE_dict: keys = filepath of mha files corresponding to PACE plans, values = [image id number, plan]
+    #2. PRISM_dict: keys = filepath of mha files corresponding to PRISM plans, values = [image_id number, plan]
 
 def metadata_from_dose_dir(dose_dir):
     files = np.array([os.path.join(dose_dir, fl) for fl in os.listdir(dose_dir) if "mha" in fl])
-    id_dict = {}
+    PACE_dict = {}
+    PRISM_dict = {}
     for file in files:
     # Extract image id_number to which dose corresponds to
         reader = sitk.ImageFileReader()
@@ -22,8 +24,13 @@ def metadata_from_dose_dir(dose_dir):
         plan = reader.GetMetaData('Plan')
 
         #Populate dictionary: key = filename, value = image id number
-        id_dict[file] = [im_id, plan]
-    return id_dict
+        if 'PACE' in plan:
+            PACE_dict[file] = [im_id, plan]
+        elif 'PRISM' in plan:
+            PRISM_dict[file] = [im_id, plan]
+    return PACE_dict, PRISM_dict
 
+dose_dir = '/Users/sblackledge/Documents/ProKnow_database/NIHR_1/dose'
 
+PACE_dict, PRISM_dict = metadata_from_dose_dir(dose_dir)
 
