1de4bf8a907511a1bbcde3d29b916834f1875184
Sarah Blackledge <sblackledge@25283radlt.icr.ac.uk>
2022-05-24 14:26:53 +0100

    reversed dictionary so that keys are image id number and value is corresponding mha filepath

--- metadata_from_dose_dir.py
+++ metadata_from_dose_dir.py
@@ -2,19 +2,20 @@ import numpy as np
 import os
 import SimpleITK as sitk
 
-#Create separate dictionaries for PACE and PRISM plans within user-specified directory with filepath and relevant metadata as key-value pairs
+#Create separate dictionaries for PACE and PRISM plans within user-specified directory with id number and filepath as key-value pairs
 #inputs:
     #1. dose_dir: string - full file path of directory where mha files are stored
 
 #Output:
-    #1. PACE_dict: keys = filepath of mha files corresponding to PACE plans, values = [image id number, plan]
-    #2. PRISM_dict: keys = filepath of mha files corresponding to PRISM plans, values = [image_id number, plan]
+    #1. PACE_dict: keys = image id number, value = [fpath of mha file]
+    #2. PRISM_dict: keys = image id number, value = [fpath of mha file]
 
 def metadata_from_dose_dir(dose_dir):
-    files = np.array([os.path.join(dose_dir, fl) for fl in os.listdir(dose_dir) if "mha" in fl])
+    mha_files = np.array([os.path.join(dose_dir, fl) for fl in os.listdir(dose_dir) if "mha" in fl])
     PACE_dict = {}
     PRISM_dict = {}
-    for file in files:
+
+    for file in mha_files:
     # Extract image id_number to which dose corresponds to
         reader = sitk.ImageFileReader()
         reader.SetFileName(file)
@@ -23,14 +24,14 @@ def metadata_from_dose_dir(dose_dir):
         im_id = reader.GetMetaData('OnImage')
         plan = reader.GetMetaData('Plan')
 
-        #Populate dictionary: key = filename, value = image id number
+
+        #Populate dictionary: key = image id number, value = fpath of corresponding mha file
         if 'PACE' in plan:
-            PACE_dict[file] = [im_id, plan]
+            PACE_dict[im_id] = [file]
         elif 'PRISM' in plan:
-            PRISM_dict[file] = [im_id, plan]
+            PRISM_dict[im_id] = [file]
     return PACE_dict, PRISM_dict
 
-dose_dir = '/Users/sblackledge/Documents/ProKnow_database/NIHR_1/dose'
-
 PACE_dict, PRISM_dict = metadata_from_dose_dir(dose_dir)
+
 
