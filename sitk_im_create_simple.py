bbb7342cd36f010dcff094f36658aa7013da2671
Sarah Blackledge <sblackledge@25283radlt.icr.ac.uk>
2022-05-26 13:13:19 +0100

    creates sitk image object from directory of dicom files. Simpler implementation than original sitk_im_create.py

---
+++ sitk_im_create_simple.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pydicom as dicom
+import SimpleITK as sitk
+import os
+
+
+def sitk_im_create_simple(im_str, dcm_dir):
+    files = np.array([os.path.join(dcm_dir, fl) for fl in os.listdir(dcm_dir) if "dcm" in fl and im_str in fl])
+    dicoms = np.array([dicom.read_file(fl, stop_before_pixels = True) for fl in files])
+    locations = np.array([float(dcm.ImagePositionPatient[-1]) for dcm in dicoms])
+    files = files[np.argsort(locations)]
+    sitk_im = sitk.ReadImage(files)
+
+    return sitk_im
