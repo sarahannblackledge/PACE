58b41b416c4ef512264392379616f49ed2356724
Sarah Blackledge <sblackledge@25283radlt.icr.ac.uk>
2022-09-13 11:22:43 +0100

    normalizes and saves nifti image files (mri) to 2D tiff files. Orientation can be specified to be axial or sagittal

--- preprocess_data_2D_axial.py
+++ preprocess_data_2D_axial.py
@@ -27,69 +27,66 @@ def normalize_im(fpath_nifti, norm_type):
 
     return(rescaled_arr)
 
-def arr_to_png(rescaled_arr, save_dir, fpath_nifti):
-    #transpose to conventional slice order
-    arr = np.array(rescaled_arr).transpose((2, 1, 0))
-    arr = np.rot90(arr, k=-1, axes=(0, 1))
+'''inputs:
+    1. rescaled_arr: 3D numpy array in desired grayscale vals
+    2. save_dir: str - directory where 2D image files should be saved.
+    3. fpath_nifti: str - full filename of original nifti file to be converted into 2D image files
+    4. orientation: str - either 'axial' or 'sagittal'. Indicates which orientation images should be saved in.'''
+
+def arr_to_png(rescaled_arr, save_dir, fpath_nifti, orientation):
 
     #get first part of filename
     f1 = Path(fpath_nifti).stem
 
-    for i in range(arr.shape[-1]):
-        ax_slice = arr[:, :, i].astype('uint8')
-        savename = f1 + '_' + str(i) + '.png'
-        savepath = os.path.join(save_dir, savename)
+    if orientation == 'axial':
 
-        cv2.imwrite(savepath, ax_slice)
+        for i in range(rescaled_arr.shape[0]):
+            im_slice = rescaled_arr[i, :, :].astype('uint8')
+            savename = f1 + '_' + str(i) + '.tiff'
+            savepath = os.path.join(save_dir, savename)
+
+            cv2.imwrite(savepath, im_slice)
+            #np.save(savepath, im_slice)
+
+    elif orientation == 'sagittal':
+
+        for i in range(rescaled_arr.shape[2]):
+            im_slice = rescaled_arr[:, :, i].astype('uint8')
+            savename = f1 + '_' + str(i) + '.tiff'
+            savepath = os.path.join(save_dir, savename)
+
+            cv2.imwrite(savepath, im_slice)
 
         #display
         '''if i == 150:
             plt.figure()
-            plt.imshow(ax_slice, cmap='gray')
+            plt.imshow(im_slice, cmap='gray')
             plt.show()'''
-
-#Loop through all images for one patientß
-save_dir_ims = '/Users/sblackledge/Documents/ProKnow_database/axial_data/all_images'
-save_dir_masks = '/Users/sblackledge/Documents/ProKnow_database/axial_data/all_masks'
-
-#adjust per image-dose pair
-patient = 'NIHR_14'
-ids = [21, 23, 34, 47, 57, 82, 94, 14]
-
-for id in ids:
-
-    #hard-coded variables (consistent in Proknow database directory)
-    id_str = 'MR' + str(id)
-    fpath_nifti = '/Users/sblackledge/Documents/ProKnow_database/nifti_dump_full/MRI/' + patient + '_' + id_str + '.nii'
-    fpath_nifti_mask = '/Users/sblackledge/Documents/ProKnow_database/nifti_dump_full/masks_multiclass/' + patient + '_' + id_str + '.nii'
-    id_num = str(id)
-    if not os.path.isfile(fpath_nifti):
-        id_str = 'RefMR' + str(id)
-        fpath_nifti = '/Users/sblackledge/Documents/ProKnow_database/nifti_dump_full/MRI/' + patient + '_' + id_str + '.nii'
-        fpath_nifti_mask = '/Users/sblackledge/Documents/ProKnow_database/nifti_dump_full/masks_multiclass/' + patient + '_' + id_str + '.nii'
-        id_num = id_str
-
-    #images
-    rescaled_arr = normalize_im(fpath_nifti, 'z-score')
-    arr_to_png(rescaled_arr, save_dir_ims, fpath_nifti)
-
-    #masks
-    rescaled_mask = normalize_im(fpath_nifti_mask, 'None')
-    arr_to_png(rescaled_mask, save_dir_masks, fpath_nifti_mask)
-
-
 
 
 # Single file example
-'''fpath_nifti = '/Users/sblackledge/Documents/ProKnow_database/nifti_dump_full/MRI/NIHR_1_MR97.nii'
-fpath_nifti_mask = '/Users/sblackledge/Documents/ProKnow_database/nifti_dump_full/masks_multiclass/NIHR_1_MR97.nii'
-save_dir_ims = '/Users/sblackledge/Documents/ProKnow_database/axial_data/all_images'
-save_dir_masks = '/Users/sblackledge/Documents/ProKnow_database/axial_data/all_masks'
+'''fpath_nifti = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/nifti_dump/MRI/NIHR_1_MR11.nii'
+save_dir_ims = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/pngs/all_images'
 
 # images
 rescaled_arr = normalize_im(fpath_nifti, 'z-score')
-arr_to_png(rescaled_arr, save_dir_ims, fpath_nifti)
+arr_to_png(rescaled_arr, save_dir_ims, fpath_nifti)'''
 
 #masks
+'''fpath_nifti_mask = '/Users/sblackledge/Documents/ProKnow_database/nifti_dump_full/masks_multiclass/NIHR_1_MR97.nii'
+save_dir_masks = '/Users/sblackledge/Documents/ProKnow_database/axial_data/all_masks'
 rescaled_mask = normalize_im(fpath_nifti_mask, 'None')
 arr_to_png(rescaled_mask, save_dir_masks, fpath_nifti_mask)'''
+
+
+#Loop through all images for one patient
+save_dir_ims = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/tiffs/all_sag_masks_SVs'
+mr_dir = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/nifti_dump/masks3D/SVsOnly'
+for file in os.listdir(mr_dir):
+    if file.endswith(".nii"):
+        fpath_nifti = os.path.join(mr_dir, file)
+        rescaled_arr = normalize_im(fpath_nifti, 'None')
+        arr_to_png(rescaled_arr, save_dir_ims, fpath_nifti, 'sagittal')
+
+
+
