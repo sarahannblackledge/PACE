7a4f9dd29431ea0b9abb67af3e5d0981266a09f3
Sarah Blackledge <sblackledge@25283radlt.icr.ac.uk>
2022-09-13 11:23:54 +0100

    splits all data in specified folder into training, test, and validation sub-folders based on proportions specified by user. Can set seed to replicate for images and labels. Ensures patients are not mixed between sub-folders

--- split_data_SB.py
+++ split_data_SB.py
@@ -35,29 +35,29 @@ def split_data_AI(desired_ids, source_dir, destination_dir):
 
 n_training = 11
 n_val = 2
-test_dir_img = '/Users/sblackledge/Documents/ProKnow_database/axial_data/test/images/img'
-test_dir_mask = '/Users/sblackledge/Documents/ProKnow_database/axial_data/test/labels/img'
+test_dir_img = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/tiffs/test/sag_images'
+test_dir_mask = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/tiffs/test/sag_labels'
 
-training_dir_img = '/Users/sblackledge/Documents/ProKnow_database/axial_data/train/images/img'
-training_dir_mask = '/Users/sblackledge/Documents/ProKnow_database/axial_data/train/labels/img'
+training_dir_img = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/tiffs/train/sag_images'
+training_dir_mask = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/tiffs/train/sag_labels'
 
-val_dir_img = '/Users/sblackledge/Documents/ProKnow_database/axial_data/validation/images/img'
-val_dir_mask = '/Users/sblackledge/Documents/ProKnow_database/axial_data/validation/labels/img'
+val_dir_img = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/tiffs/validation/sag_images'
+val_dir_mask = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/tiffs/validation/sag_labels'
 
-source_dir_img = '/Users/sblackledge/Documents/ProKnow_database/axial_data/all_images'
-source_dir_mask = '/Users/sblackledge/Documents/ProKnow_database/axial_data/all_masks'
+source_dir_img = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/tiffs/all_sag_images'
+source_dir_mask = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/tiffs/all_sag_masks_SVs'
 
 training_ids, val_ids, test_ids = allocate_ids(n_training, n_val)
 
 #Populate training folders
 split_data_AI(training_ids, source_dir_img, training_dir_img)
-#split_data_AI(training_ids, source_dir_mask, training_dir_mask)
+split_data_AI(training_ids, source_dir_mask, training_dir_mask)
 
 #Populate validation folders
-#split_data_AI(val_ids, source_dir_img, val_dir_img)
-#split_data_AI(val_ids, source_dir_mask, val_dir_mask)
+split_data_AI(val_ids, source_dir_img, val_dir_img)
+split_data_AI(val_ids, source_dir_mask, val_dir_mask)
 
 #Populate test folders
-#split_data_AI(test_ids, source_dir_img, test_dir_img)
-#split_data_AI(test_ids, source_dir_mask, test_dir_mask)
+split_data_AI(test_ids, source_dir_img, test_dir_img)
+split_data_AI(test_ids, source_dir_mask, test_dir_mask)
 
