c381ae00043abc17a3860ca4b23f82de7624b947
Sarah Blackledge <sblackledge@25283radlt.icr.ac.uk>
2022-09-29 09:17:42 +0100

    working version

--- split_data_SB.py
+++ split_data_SB.py
@@ -35,29 +35,29 @@ def split_data_AI(desired_ids, source_dir, destination_dir):
 
 n_training = 11
 n_val = 2
-test_dir_img = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/tiffs/test/sag_images'
-test_dir_mask = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/tiffs/test/sag_labels'
+test_dir_img = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/nifti_cropped/test/images'
+test_dir_mask = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/nifti_cropped/test/bowel_labels'
 
-training_dir_img = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/tiffs/train/sag_images'
-training_dir_mask = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/tiffs/train/sag_labels'
+training_dir_img = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/nifti_cropped/training/images'
+training_dir_mask = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/nifti_full_TVTsets/training/prostate_labels'
 
-val_dir_img = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/tiffs/validation/sag_images'
-val_dir_mask = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/tiffs/validation/sag_labels'
+val_dir_img = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/nifti_cropped/validation/images'
+val_dir_mask = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/nifti_cropped/validation/bowel_labels/'
 
-source_dir_img = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/tiffs/all_sag_images'
-source_dir_mask = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/tiffs/all_sag_masks_SVs'
+source_dir_img = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/nifti_dump/MRI'
+source_dir_mask = '/Users/sblackledge/Documents/ProKnow_database/RMH_proknow/proknowPACE/nifti_dump/masks3D_no_overlap/ProstateOnly'
 
 training_ids, val_ids, test_ids = allocate_ids(n_training, n_val)
 
 #Populate training folders
-split_data_AI(training_ids, source_dir_img, training_dir_img)
+#split_data_AI(training_ids, source_dir_img, training_dir_img)
 split_data_AI(training_ids, source_dir_mask, training_dir_mask)
 
 #Populate validation folders
-split_data_AI(val_ids, source_dir_img, val_dir_img)
-split_data_AI(val_ids, source_dir_mask, val_dir_mask)
+#split_data_AI(val_ids, source_dir_img, val_dir_img)
+#split_data_AI(val_ids, source_dir_mask, val_dir_mask)
 
 #Populate test folders
-split_data_AI(test_ids, source_dir_img, test_dir_img)
-split_data_AI(test_ids, source_dir_mask, test_dir_mask)
+#split_data_AI(test_ids, source_dir_img, test_dir_img)
+#split_data_AI(test_ids, source_dir_mask, test_dir_mask)
 
