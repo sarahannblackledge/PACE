9445dfe7483b9634992a92b7f34150ae34a113b8
Sarah Blackledge <sblackledge@25283radlt.icr.ac.uk>
2024-08-08 14:15:03 +0100

    updated to add bowel subplots

--- generate_histogram_figure.py
+++ generate_histogram_figure.py
@@ -37,20 +37,26 @@ df_verif = pd.read_excel(fpath_xlsx, sheet_name='Verif')
 #Hard-code cut-off thresholds, dose stat name, and column index corresponding to excel file as dictionary values
 #Bowel always 0, so eliminate from dictionary to avoid useless subplot
 cutoff_dict = {
-    1: [[90], 'CTVpsv_V4000', 1, 'Volume (%) of CTV covered by 40Gy'],
-    2: [[90], 'PTVpsv_V3625', 2, 'Volume (%) of PTV covered by 36.25Gy'],
-    3: [[3371.2], 'PTVpsv_D3625', 3, 'Dose (Gy) covering 98% of PTV'],
-    4: [[10], 'Bladder_V3700', 4, 'Volume (cc) of bladder covered by 37Gy'],
+    1: [[85], 'CTVpsv_V4000', 1, 'Volume (%) of CTV covered by 40Gy'],
+    2: [[85], 'PTVpsv_V3625', 2, 'Volume (%) of PTV covered by 36.25Gy'],
+    3: [[3371.2], 'PTVpsv_D3625*', 3, 'Dose (Gy) covering 98% of PTV'],
+    4: [[10], 'Bladder_V3700*', 4, 'Volume (cc) of bladder covered by 37Gy'],
     5: [[40], 'Bladder_V1810', 5, 'Volume (%) of bladder covered by 18.1Gy'],
-    6: [[2], 'Rectum_V3600', 6, 'Volume (cc) of Rectum covered by 36Gy'],
+    6: [[2], 'Rectum_V3600*', 6, 'Volume (cc) of Rectum covered by 36Gy'],
     7: [[20], 'Rectum_V2900', 7, 'Volume (%) of Rectum covered by 29Gy'],
-    8: [[50], 'Rectum_V1810', 8, 'Volume (%) of Rectum covered by 18.1Gy']
+    8: [[50], 'Rectum_V1810', 8, 'Volume (%) of Rectum covered by 18.1Gy'],
+    9: [[1], 'Bowel_V3000', 9, 'Volume (cc) of bowel covered by 30Gy'],
+    10: [[5], 'Bowel_V1810', 10, 'Volume (cc) of bowel covered by 18.1Gy']
 }
 
 # Initialize figure with shared y-axis
-fig, axes = plt.subplots(2, 4, figsize=(19, 10), sharey=False)
+fig, axes = plt.subplots(3, 4, figsize=(19, 15), sharey=False)
 plt.subplots_adjust(hspace=0.5)
-fig.suptitle("Comparison of dose statistics: Session vs. Verif", fontsize=20)
+fig.suptitle("Distribution of dose statistics for Target and OAR structures", fontsize=20)
+
+# Initialize lists to collect handles and labels
+all_handles = []
+all_labels = []
 
 # Loop through dose stats
 for n, (ax, ticker) in enumerate(zip(axes.flatten(), cutoff_dict)):
@@ -61,33 +67,50 @@ for n, (ax, ticker) in enumerate(zip(axes.flatten(), cutoff_dict)):
     dose_stat_sess, dose_stat_verif, bins, kde_sess, kde_verif, xx, q1q2q3_verif = extract_hist_vals(index, df_session, df_verif)
 
     # Add histograms
-    ax.hist(dose_stat_sess, density=True, bins=bins, alpha=0.5, label='Session', color='green', histtype='stepfilled')
-    ax.hist(dose_stat_verif, density=True, bins=bins, alpha=0.2, label='Verif', color='purple', histtype='stepfilled')
+    ax.hist(dose_stat_sess, density=True, bins=bins, alpha=0.5, label='Session', color='gray', histtype='stepfilled')
+    ax.hist(dose_stat_verif, density=True, bins=bins, alpha=0.2, label='Verification', color='green', histtype='stepfilled')
 
-    #Add KDE plots
-    #ax.plot(xx, kde_sess(xx), color='green')
-    #ax.plot(xx, kde_verif(xx), color='purple')
+    # Add KDE plots
+    # ax.plot(xx, kde_sess(xx), color='green')
+    # ax.plot(xx, kde_verif(xx), color='purple')
 
     # Add dashed vertical line at the cut-off value (clinical goals)
     colors = ['black', 'blue', 'brown']
     cutoffs = vals[0]
     for index, c_val in enumerate(cutoffs):
-        ax.axvline(c_val, color=colors[index], linestyle='dashed', linewidth=1, label='clinical goal')
+        ax.axvline(c_val, color=colors[index], linestyle='dashed', linewidth=1, label='Clinical goal')
 
-    #Add dashed vertical line at first quartile (targets) and third quartile (OARS) of verification data
-    print(q1q2q3_verif)
+    # Add dashed vertical line at first quartile (targets) and third quartile (OARS) of verification data
     if n <= 2:
-        ax.axvline(q1q2q3_verif[0], color='purple', linestyle='dashed', linewidth=1)
+        ax.axvline(q1q2q3_verif[0], color='green', linestyle='dashed', linewidth=1)
     else:
-        ax.axvline(q1q2q3_verif[2], color='purple', linestyle='dashed', linewidth=1, label='IQR verif')
+        ax.axvline(q1q2q3_verif[2], color='green', linestyle='dashed', linewidth=1, label='Verif 25% (targets) or 75% (OARs)')
 
-
-    #Set title and x-axis labels for each subplot
+    # Set title and x-axis labels for each subplot
     ax.set_title(vals[1])
     ax.set_xlabel(vals[3])
+
+    # Collect handles and labels
+    handles, labels = ax.get_legend_handles_labels()
+    all_handles.extend(handles)
+    all_labels.extend(labels)
+
+# Hide unwanted axes (last two axes)
+for ax in axes.flatten()[10:]:
+    fig.delaxes(ax)
+
+# Remove duplicates in handles and labels
+unique_labels_handles = dict(zip(all_labels, all_handles))
+unique_handles = list(unique_labels_handles.values())
+unique_labels = list(unique_labels_handles.keys())
+
+# Add a legend in the empty space where the last two axes were
+legend_ax = fig.add_subplot(3, 4, 11)
+legend_ax.axis('off')  # Hide the axis
+legend_ax.legend(unique_handles, unique_labels, loc='upper left', frameon=False, fontsize=18)
 
 # Set common y-axis label
 fig.text(0.04, 0.5, 'Probability Density', va='center', rotation='vertical', fontsize=18)
 
-plt.legend()
+# Show the plot
 plt.show()
